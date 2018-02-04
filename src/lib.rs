464f251d99b310c5f00416b536d897c716801da6
Aleksey Suslov <aleksey.suslov@strikead.com>
2018-02-04 22:27:24 +0100

    Add nltk data

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,3 @@
 pub mod nltk;
+pub mod spark;
 pub mod sklearn;
--- src/spark/mod.rs
+++ src/spark/mod.rs
@@ -1,22 +1,16 @@
 use std::str::Lines;
 
 pub enum Language {
-    Arabic,
-    Azerbaijani,
     Danish,
     Dutch,
     English,
     Finnish,
     French,
     German,
-    Greek,
     Hungarian,
     Italian,
-    Kazakh,
-    Nepali,
     Norwegian,
     Portuguese,
-    Romanian,
     Russian,
     Spanish,
     Swedish,
@@ -25,22 +19,16 @@ pub enum Language {
 
 pub fn load(language: Language) -> Lines<'static> {
     let text = match language {
-        Language::Arabic => include_str!("data/arabic.txt"),
-        Language::Azerbaijani => include_str!("data/azerbaijani.txt"),
         Language::Danish => include_str!("data/danish.txt"),
         Language::Dutch => include_str!("data/dutch.txt"),
         Language::English => include_str!("data/english.txt"),
         Language::Finnish => include_str!("data/finnish.txt"),
         Language::French => include_str!("data/french.txt"),
         Language::German => include_str!("data/german.txt"),
-        Language::Greek => include_str!("data/greek.txt"),
         Language::Hungarian => include_str!("data/hungarian.txt"),
         Language::Italian => include_str!("data/italian.txt"),
-        Language::Kazakh => include_str!("data/kazakh.txt"),
-        Language::Nepali => include_str!("data/nepali.txt"),
         Language::Norwegian => include_str!("data/norwegian.txt"),
         Language::Portuguese => include_str!("data/portuguese.txt"),
-        Language::Romanian => include_str!("data/romanian.txt"),
         Language::Russian => include_str!("data/russian.txt"),
         Language::Spanish => include_str!("data/spanish.txt"),
         Language::Swedish => include_str!("data/swedish.txt"),
@@ -65,18 +53,6 @@ mod tests {
     }
 
     #[test]
-    fn arabic() {
-        assert_count(Language::Arabic, 248);
-        assert_head(Language::Arabic, vec!("إذ", "إذا", "إذما", "إذن", "أف"));
-    }
-
-    #[test]
-    fn azerbaijani() {
-        assert_count(Language::Azerbaijani, 165);
-        assert_head(Language::Azerbaijani, vec!("a", "ad", "altı", "altmış", "amma"));
-    }
-
-    #[test]
     fn danish() {
         assert_count(Language::Danish, 94);
         assert_head(Language::Danish, vec!("og", "i", "jeg", "det", "at"));
@@ -90,7 +66,7 @@ mod tests {
 
     #[test]
     fn english() {
-        assert_count(Language::English, 179);
+        assert_count(Language::English, 181);
         assert_head(Language::English, vec!("i", "me", "my", "myself", "we"));
     }
 
@@ -102,7 +78,7 @@ mod tests {
 
     #[test]
     fn french() {
-        assert_count(Language::French, 155);
+        assert_count(Language::French, 156);
         assert_head(Language::French, vec!("au", "aux", "avec", "ce", "ces"));
     }
 
@@ -110,12 +86,6 @@ mod tests {
     fn german() {
         assert_count(Language::German, 231);
         assert_head(Language::German, vec!("aber", "alle", "allem", "allen", "aller"));
-    }
-
-    #[test]
-    fn greek() {
-        assert_count(Language::Greek, 265);
-        assert_head(Language::Greek, vec!("αλλα", "αν", "αντι", "απο", "αυτα"));
     }
 
     #[test]
@@ -131,18 +101,6 @@ mod tests {
     }
 
     #[test]
-    fn kazakh() {
-        assert_count(Language::Kazakh, 380);
-        assert_head(Language::Kazakh, vec!("ах", "ох", "эх", "ай", "эй"));
-    }
-
-    #[test]
-    fn nepali() {
-        assert_count(Language::Nepali, 255);
-        assert_head(Language::Nepali, vec!("छ", "र", "पनि", "छन्", "लागि"));
-    }
-
-    #[test]
     fn norwegian() {
         assert_count(Language::Norwegian, 176);
         assert_head(Language::Norwegian, vec!("og", "i", "jeg", "det", "at"));
@@ -152,12 +110,6 @@ mod tests {
     fn portuguese() {
         assert_count(Language::Portuguese, 203);
         assert_head(Language::Portuguese, vec!("de", "a", "o", "que", "e"));
-    }
-
-    #[test]
-    fn romanian() {
-        assert_count(Language::Romanian, 356);
-        assert_head(Language::Romanian, vec!("a", "abia", "acea", "aceasta", "această"));
     }
 
     #[test]
