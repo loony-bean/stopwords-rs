35071e53054e46b0c9bce5eaa4c1ba07185dc7be
Aleksey Suslov <aleksey.suslov@strikead.com>
2018-02-06 22:40:52 +0100

    Add from_str

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 #[macro_use]
 extern crate lazy_static;
 
+use std::str::FromStr;
+
 pub mod nltk;
 pub mod spark;
 pub mod sklearn;
@@ -9,7 +11,7 @@ pub use nltk::NLTK;
 pub use spark::Spark;
 pub use sklearn::SkLearn;
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, PartialEq, Debug)]
 pub enum Language {
     Arabic,
     Azerbaijani,
@@ -33,6 +35,47 @@ pub enum Language {
     Turkish,
 }
 
+impl FromStr for Language {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "arabic" => Ok(Language::Arabic),
+            "azerbaijani" => Ok(Language::Azerbaijani),
+            "danish" => Ok(Language::Danish),
+            "dutch" => Ok(Language::Dutch),
+            "english" => Ok(Language::English),
+            "finnish" => Ok(Language::Finnish),
+            "french" => Ok(Language::French),
+            "german" => Ok(Language::German),
+            "greek" => Ok(Language::Greek),
+            "hungarian" => Ok(Language::Hungarian),
+            "italian" => Ok(Language::Italian),
+            "kazakh" => Ok(Language::Kazakh),
+            "nepali" => Ok(Language::Nepali),
+            "norwegian" => Ok(Language::Norwegian),
+            "portuguese" => Ok(Language::Portuguese),
+            "romanian" => Ok(Language::Romanian),
+            "russian" => Ok(Language::Russian),
+            "spanish" => Ok(Language::Spanish),
+            "swedish" => Ok(Language::Swedish),
+            "turkish" => Ok(Language::Turkish),
+            _ => Err(())
+        }
+    }
+}
+
 pub trait Stopwords {
     fn stopwords(language: Language) -> Option<&'static [&'static str]>;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn from_str() {
+        assert_eq!(Language::from_str("english").ok(), Some(Language::English));
+        assert_eq!(Language::from_str("en").ok(), None);
+    }
 }
